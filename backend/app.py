2d6d51ac00e8a9c1741d7bf3fc31393d26e21664
Waleed Haroon <waleedharoon2002@gmail.com>
2025-07-03 20:25:51 +0500

    trying

--- backend/app.py
+++ backend/app.py
@@ -64,4 +64,6 @@ def feedback():
     return jsonify({'message': 'Feedback received!'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port) 
